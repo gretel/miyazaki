e5a456c8afb3a4e258fa2f2193f0ebef32e8a501
Tom Hensel <code@jitter.eu>
2026-05-04 03:10:22 +0200

    package/pluto-system-top-bit: bump v0.38 -> v0.39
    Signed-off-by: Tom Hensel <code@jitter.eu>

--- package/pluto-system-top-bit/pluto-system-top-bit.mk
+++ package/pluto-system-top-bit/pluto-system-top-bit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PLUTO_SYSTEM_TOP_BIT_VERSION = v0.38
+PLUTO_SYSTEM_TOP_BIT_VERSION = v0.39
 PLUTO_SYSTEM_TOP_BIT_SOURCE = plutosdr-jtag-bootstrap-$(PLUTO_SYSTEM_TOP_BIT_VERSION).zip
 PLUTO_SYSTEM_TOP_BIT_SITE = http://github.com/analogdevicesinc/plutosdr-fw/releases/download/$(PLUTO_SYSTEM_TOP_BIT_VERSION)
 
