f8a34f41572d71362ae03b53bb34a009ae02725e
Tom Hensel <code@jitter.eu>
2026-05-04 03:09:28 +0200

    package/gr-satellite: pin to daniestevez@9110aaef
    Replace 'main' branch tracking with the current branch HEAD so the
    Buildroot download cache and CI archive cache invalidate correctly when
    upstream advances.
    
    daniestevez/gr-satellites@9110aaef (2026-04-15) is main HEAD.
    
    Signed-off-by: Tom Hensel <code@jitter.eu>

--- package/gr-satellite/grsatellite.mk
+++ package/gr-satellite/grsatellite.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 
-GR_SATELLITE_VERSION = main
+GR_SATELLITE_VERSION = 9110aaefb0bdc27d4fde414bcc34c3670d916638
 
 GR_SATELLITE_SITE = $(call github,daniestevez,gr-satellites,$(GR_SATELLITE_VERSION))
 GR_SATELLITE_STAGING = YES
