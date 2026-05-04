addd3657ea210716b58620f70c5f6657a028b858
Tom Hensel <code@jitter.eu>
2026-05-04 03:09:37 +0200

    package/srt: pin to Haivision@b6b4ae99 + rename srt-master.mk to srt.mk
    Replace 'master' branch tracking with the current branch HEAD so the
    Buildroot download cache and CI archive cache invalidate correctly when
    upstream advances.
    
    Haivision/srt@b6b4ae99 (2026-04-17) is master HEAD; the same commit is
    also tagged as v1.5.5 (released 2026-04-28).
    
    Rename srt-master.mk -> srt.mk: the '-master' suffix only made sense
    when the file tracked the master branch literally; it's now a SHA pin
    and the filename should reflect the package, not its (former) ref.
    
    Signed-off-by: Tom Hensel <code@jitter.eu>

--- package/srt/srt.mk
+++ package/srt/srt.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 
-SRT_VERSION = master
+SRT_VERSION = b6b4ae990daa8193625a4ddeaeaed03023b23125
 SRT_SITE = $(call github,Haivision,srt,$(SRT_VERSION))
 SRT_INSTALL_STAGING = YES
 SRT_CONF_OPTS = -DENABLE_ENCRYPTION=OFF -DENABLE_STATIC=OFF
